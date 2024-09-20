af180107c2fbd42495bf36d981f37f94d3a8b40f
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-09-20 21:55:21 +0600

    mission accomplished 🔥🚀🔥

--- food_shop_server/settings.py
+++ food_shop_server/settings.py
@@ -27,7 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = ['https://food-backend-gjx4.onrender.com','https://*.127.0.0.1']
-
+SESSION_ENGINE = 'django.contrib.sessions.backends.db'
 
 # Application definition
 
