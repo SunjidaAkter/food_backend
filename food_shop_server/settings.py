9dd070758d7fb3fdd10de1a9570d1c7095dff515
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-09-24 17:38:22 +0600

    mission accomplished 🔥🚀🔥

--- food_shop_server/settings.py
+++ food_shop_server/settings.py
@@ -90,8 +90,12 @@ WSGI_APPLICATION = 'food_shop_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER':'postgres.ylxtxqqpchljdnlkjrdo',
+        'PASSWORD': 'qhjOtsSURREwTiuS',
+        'HOST': 'aws-0-ap-southeast-1.pooler.supabase.com',
+        'PORT': '6543',
     }
 }
 
