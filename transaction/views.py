8486bd89e1246ba1ef39315f6a82a7811cce2f03
SunjidaAkter <sanjidabintesayid7@gmail.com>
2024-11-01 11:53:53 +0600

    mission accomplished 🔥🚀🔥

--- transaction/views.py
+++ transaction/views.py
@@ -77,9 +77,9 @@ class TransactionSerializer(serializers.Serializer):
                     'total_amount': amount,
                     'currency': "BDT",
                     'tran_id': transaction_id,
-                    'success_url': 'https://food-backend-zeta.vercel.app/transaction/success/', 
-                    'fail_url': 'https://food-backend-zeta.vercel.app/transaction/fail/',
-                    'cancel_url': 'https://food-backend-zeta.vercel.app/transaction/cancel/',
+                    'success_url': 'http://127.0.0.1:8000/transaction/success/', 
+                    'fail_url': 'http://127.0.0.1:8000/transaction/fail/',
+                    'cancel_url': 'http://127.0.0.1:8000/transaction/cancel/',
                     'emi_option': 0,
                     'cus_name': user_accounts.user.first_name,
                     'cus_email': user_accounts.user.email,
@@ -106,7 +106,10 @@ class TransactionSerializer(serializers.Serializer):
                             customer=user_accounts,
                             amount=amount,
                             trans_type=trans_type,
+                            payment_url=gateway_url
                         )
+                        # transaction.payment_url = gateway_url
+                        # transaction.save(update_fields=['payment_url'])
                         trans_id = transactions.id
                         arr.append(trans_id)
                         global_data[transaction_id] = user_accounts
@@ -130,6 +133,19 @@ class AllUserTransactionListAPIView(generics.ListAPIView):
     # queryset = Booking.objects.all()
     queryset = UserTransaction.objects.all()
     serializer_class = AllUserTransactionSerializer
+    def get_queryset(self):
+        customer_id = self.request.query_params.get('customer_id')
+
+        # If customer_id is not provided, return the full queryset
+        if customer_id is None:
+            return super().get_queryset()
+
+        # Check if customer_id is a valid integer
+        if not customer_id.isdigit():
+            raise ValidationError({"customer_id": "Invalid customer_id. It must be a number."})
+
+        # Filter queryset by customer_id
+        return super().get_queryset().filter(customer_id=int(customer_id))
     
 
 class TransactionViewSet(viewsets.ViewSet):
@@ -164,22 +180,25 @@ def transaction_success(request):
 
             # payment.status = 'COMPLETED'
             # payment.save(update_fields=['status'])
+            print(transaction_id)
             trans.transaction_id = transaction_id
             trans.payment_status="Completed"
-            if trans.trans_type == 'Deposit':
+            if trans.trans_type == "Deposit":
                 trans.balance_after_trans=user_accounts.amount+trans.amount
                 user_accounts.amount=user_accounts.amount+trans.amount
-            if trans.trans_type == 'Withdraw':
+            if trans.trans_type == "Withdraw":
                 trans.balance_after_trans=user_accounts.amount-trans.amount
                 user_accounts.amount=user_accounts.amount-trans.amount
+            trans.save(update_fields=['transaction_id'])
+            trans.save(update_fields=['balance_after_trans'])
             trans.save(update_fields=['payment_status'])
             user_accounts.save(update_fields=['amount'])
             email_subject = _("Payment Confirmation")
-            email_body = render_to_string('trans_confirm_email.html', {
+            email_body = render_to_string('transaction_confirm_email.html', {
                 'trans': trans,
                 # 'payment': payment,
                 'user': user_accounts,
-                'total_cost': trans.cost,
+                'total_cost': trans.amount,
                 'pdf_link': request.build_absolute_uri(reverse('download_order_pdf', args=[trans.id]))
             })
             email = EmailMultiAlternatives(email_subject, '', to=[user_accounts.user.email])
